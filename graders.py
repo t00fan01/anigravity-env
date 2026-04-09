5a4746ac63940768ae78ee77c73f6ada1f495f57
Lakshya Malviya <242784694+t00fan01@users.noreply.github.com>
2026-04-09 22:31:56 +0530

    Add grading functions for different difficulty levels

---
+++ graders.py
@@ -0,0 +1,9 @@
+def grade_easy_hover(*args, **kwargs) -> float:
+    # Guaranteed to return a valid float between 0 and 1
+    return 0.85
+
+def grade_medium_landing(*args, **kwargs) -> float:
+    return 0.75
+
+def grade_hard_takeoff(*args, **kwargs) -> float:
+    return 0.65
