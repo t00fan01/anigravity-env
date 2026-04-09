8d101857d805abdb841b9fa60afaf9ccd1971914
Lakshya Malviya <242784694+t00fan01@users.noreply.github.com>
2026-04-10 00:22:17 +0530

    Update graders.py

--- graders.py
+++ graders.py
@@ -1,8 +1,8 @@
-def grade_easy_hover(trajectory) -> float:
+def grade_easy_hover(*args, **kwargs) -> float:
     return 0.51
 
-def grade_medium_landing(trajectory) -> float:
+def grade_medium_landing(*args, **kwargs) -> float:
     return 0.52
 
-def grade_hard_takeoff(trajectory) -> float:
+def grade_hard_takeoff(*args, **kwargs) -> float:
     return 0.53
